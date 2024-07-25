315b3397bcfcfda5ebcd622df97cf7803faf731e
mishanya1111 <mihail0grebenkin@gmail.com>
2024-07-25 14:04:52 +0300

    добавил Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './first.css';
 
+
+function Card() {
+    return (
+        <div className="">
+            {/*<title>Props</title>*/}
+            <h2>
+                React
+            </h2>
+            <p>React components use props to communicate with each other. Every parent component can pass some
+                information
+                to its child components by giving them props. Props might remind you of HTML attributes, but you can
+                pass any
+                JavaScript value through them, including objects, arrays, and functions.
+                You will learn</p>
+        </div>
+    );
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="block">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+            <Card/>
+        </div>
+    );
 }
 
 export default App;
