d13cc47c36f0d9353b5aba5e00cd6a50ca22cecc
mishanya1111 <mihail0grebenkin@gmail.com>
2024-08-11 12:49:21 +0300

    переписан функционал кнопки edit, теперь checkbox не использует id, а использует ref. Также появилась вторая карточка

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './first.css';
-import {useState} from "react";
-import { MdOutlineCancel } from "react-icons/md";
-import { FaSave } from "react-icons/fa";
-import { AiOutlineEdit } from "react-icons/ai";
-function Card({baseStyle, alternativeStyle}) {
+import {useState, useRef} from "react";
+import {MdOutlineCancel} from "react-icons/md";
+import {FaSave} from "react-icons/fa";
+import {AiOutlineEdit} from "react-icons/ai";
+
+function Card({baseStyle, alternativeStyle, firstTitle, firstText}) {
     const [style, setStyle] = new useState(baseStyle);
     const [editing, setEditing] = new useState(false);
-    const [title, setTitle] = new useState('React');
-    const [text, setText] = new useState('React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions.');
-    const [button, setButton] = new useState(<>Edit<AiOutlineEdit /></>);
+    const [title, setTitle] = new useState(firstTitle);
+    const [text, setText] = new useState(firstText);
+    const [button, setButton] = new useState(<>Edit<AiOutlineEdit/></>);
+    const checkboxRef = useRef(null);
+
+
     function changeStyle() {
         setStyle((oldStyle) => oldStyle === baseStyle ? alternativeStyle : baseStyle);
     }
@@ -19,24 +23,26 @@ function Card({baseStyle, alternativeStyle}) {
     }
 
     function titleChangeHandler(event) {
-        setButton(<>Save<FaSave /></>);
+        setButton(<>Save<FaSave/></>);
         setTitle(event.target.value);
     }
 
     function textChangeHandler(event) {
-        setButton(<>Save<FaSave /></>);
+        setButton(<>Save<FaSave/></>);
         setText(event.target.value);
     }
 
     function editingHandler() {
-        const checkbox = document.getElementById('checkbox');
+
         if (!editing) {
             setButton(<>Cancel <MdOutlineCancel/></>);
-            if (style !== baseStyle) checkbox.click();
-            checkbox.style.opacity = 0;
+            if (checkboxRef.current && checkboxRef.current.checked) {
+                checkboxRef.current.checked = false;
+                setStyle(baseStyle);
+            }
         } else {
-            setButton(<>Edit<AiOutlineEdit /></>);
-            checkbox.style.opacity = 1;
+            setButton(<>Edit<AiOutlineEdit/></>);
+            //checkbox.style.opacity = 1;
         }
         changeEditing();
     }
@@ -47,12 +53,16 @@ function Card({baseStyle, alternativeStyle}) {
                 {editing ?
                     <input type='text' onChange={titleChangeHandler} value={title} style={{width: 110}}/> : title}
             </h2>
+            {editing ? "": <input type="checkbox" ref={checkboxRef}  className = "check1" onChange={changeStyle}/>  }
             <button onClick={editingHandler}
-                    style={{width: 80,
+                    style={{
+                        width: 80,
                         display: "flex",
                         alignItems: "center",
-                        justifyContent: "space-around"}}>{button}</button>
-            <input type="checkbox" id='checkbox' onChange={changeStyle}/>
+                        justifyContent: "space-around"
+                    }}>{button}</button>
+
+
         </div>
         <p className={style}>
             {editing ? <textarea onChange={textChangeHandler} value={text}
@@ -70,7 +80,12 @@ function App() {
             <img src={logo} className="AppLogo" alt="logo"/>
             <h1>Some very informative title</h1>
         </header>
-        <Card baseStyle="borderBox" alternativeStyle="yellowBox"/>
+        <Card baseStyle="borderBox" alternativeStyle="yellowBox"
+              firstTitle="React"
+              firstText="React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions."/>
+        <Card baseStyle="borderBox" alternativeStyle="yellowBox"
+              firstTitle="Proops"
+              firstText="Props is a special keyword in React that stands for properties and is being used to pass data from one component to another and mostly from parent component to child component. We can say props is a data carrier or a means to transport data. "/>
     </div>);
 }
 
