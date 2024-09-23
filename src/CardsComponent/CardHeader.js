96508c2a70765aed99b031435652255e3ddb9cbb
mishanya1111 <mihail0grebenkin@gmail.com>
2024-09-23 20:09:31 +0300

    Заработало

--- src/CardsComponent/CardHeader.js
+++ src/CardsComponent/CardHeader.js
@@ -1,58 +1,51 @@
-import {MdOutlineCancel} from "react-icons/md";
-import {FaSave} from "react-icons/fa";
-import {AiOutlineEdit} from "react-icons/ai";
+import { MdOutlineCancel } from "react-icons/md";
+import { FaSave } from "react-icons/fa";
+import { AiOutlineEdit } from "react-icons/ai";
 
 function CardHeader({
-                        value,
-                        inputChange,
-                        onCansel,
-                        onSave,
-                        onChange,
-                        onEdit,
-                        checked,
-                        editing,
-                        isDisableMode,
-                    }) {
-    return (
-        editing ? (
-            <div className={checked ? 'yellowBox' : 'borderBox'} id='divTitle'>
-                <h2>
-                    <input
-                        type='text'
-                        onChange={inputChange}
-                        value={value}
-                        style={{width: 110}}/>
-                </h2>
-                <button
-                    onClick={onCansel}
-                    className="cancelButton">Cancel<MdOutlineCancel/>
-                </button>
-                <button
-                    onClick={onSave}
-                    className="cardButton">
-                    Save<FaSave/>
-                </button>
-            </div>
-
-        ) : (
-            <div className={checked ? 'yellowBox' : 'borderBox'} id='divTitle'>
-                <h2>
-                    {value}
-                </h2>
-                <input type="checkbox" className="check1" onChange={onChange}/>
-                <div style={{width: 80}}>
-                    {!isDisableMode && (
-                        <button
-                            onClick={onEdit}
-                            className="cardButton">
-                            Edit<AiOutlineEdit/>
-                        </button>
-                    )
-                    }
-                </div>
-            </div>
-        )
-    )
+  value,
+  inputChange,
+  onCansel,
+  onSave,
+  onChange,
+  onEdit,
+  checked,
+  editing,
+  isDisableMode,
+}) {
+  return editing ? (
+    <div className={checked ? "yellowBox" : "borderBox"} id="divTitle">
+      <h2>
+        <input
+          type="text"
+          onChange={inputChange}
+          value={value}
+          style={{ width: 110 }}
+        />
+      </h2>
+      <button onClick={onCansel} className="cancelButton">
+        Cancel
+        <MdOutlineCancel />
+      </button>
+      <button onClick={onSave} className="cardButton">
+        Save
+        <FaSave />
+      </button>
+    </div>
+  ) : (
+    <div className={checked ? "yellowBox" : "borderBox"} id="divTitle">
+      <h2>{value}</h2>
+      <input type="checkbox" className="check1" onChange={onChange} />
+      <div style={{ width: 80 }}>
+        {!isDisableMode && (
+          <button onClick={onEdit} className="cardButton">
+            Edit
+            <AiOutlineEdit />
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default CardHeader;
--- src/CardsComponent/CardList.js
+++ src/CardsComponent/CardList.js
@@ -1,23 +1,20 @@
-import Card from './Card'
+import Card from "./Card";
 
-
-
-function CardList({onChecked,checkBoxChange,items}) {
-    return(
-        <>
-            {items.map((card) =>
-                <Card
-                    viewOnlyChecked={onChecked}
-                    firstTitle={card.title}
-                    firstText={card.text}
-                    key={card.id}
-                    id={card.id}
-                    checkBoxChange={checkBoxChange}
-                />
-            )
-            }
-        </>
-    )
+function CardList({ onChecked, checkBoxChange, items }) {
+  return (
+    <>
+      {items.map((card) => (
+        <Card
+          viewOnlyChecked={onChecked}
+          firstTitle={card.title}
+          firstText={card.text}
+          key={card.id}
+          id={card.id}
+          checkBoxChange={checkBoxChange}
+        />
+      ))}
+    </>
+  );
 }
 
 export default CardList;
