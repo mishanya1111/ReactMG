a077f69e8d0365158c32c71b0d186611486ad0ae
mishanya1111 <mihail0grebenkin@gmail.com>
2024-09-25 12:45:08 +0300

    4 пробела а не табуляция(файл был назван с 1 r поэтому и не работало)

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
-import logo from "./logo.svg";
-import "./first.css";
-import { useState } from "react";
-import cardsData from "./data";
-import CardList from "./CardsComponent/CardList";
+import logo from './logo.svg';
+import './first.css';
+import { useState } from 'react';
+import cardsData from './data';
+import CardList from './CardsComponent/CardList';
 
 function App() {
-  const [checked, setChecked] = useState(false);
-  const [data, setData] = useState(cardsData);
+    const [checked, setChecked] = useState(false);
+    const [data, setData] = useState(cardsData);
 
-  const changeActiveHandler = (id) => {
-    setData((cards) =>
-      cards.map((card) => {
-        if (card.id === id) {
-          const prev = { ...card };
-          prev.isActive = !card.isActive;
-          return prev;
-        }
-        return card;
-      }),
-    );
-  };
+    const changeActiveHandler = id => {
+        setData(cards =>
+            cards.map(card => {
+                if (card.id === id) {
+                    const prev = { ...card };
+                    prev.isActive = !card.isActive;
+                    return prev;
+                }
+                return card;
+            })
+        );
+    };
 
-  const deleteHandler = () => {
-    //COdeConvention?
-    setData((card) => card.filter((el) => !el.isActive));
-  };
+    const deleteHandler = () => {
+        //COdeConvention?
+        setData(card => card.filter(el => !el.isActive));
+    };
 
-  function changeCheckboxApp() {
-    setChecked(!checked);
-  }
+    function changeCheckboxApp() {
+        setChecked(!checked);
+    }
 
-  return (
-    <div>
-      <header className="AppHeader">
-        <img src={logo} className="AppLogo" alt="logo" />
-        <h1>Some very informative title</h1>
-        <div className="checkboxView">
-          <label htmlFor="checboxView">View only</label>
-          <input
-            type="checkbox"
-            onChange={changeCheckboxApp}
-            id="checboxView"
-            className="checkboxView"
-          />
+    return (
+        <div>
+            <header className="AppHeader">
+                <img src={logo} className="AppLogo" alt="logo" />
+                <h1>Some very informative title</h1>
+                <div className="checkboxView">
+                    <label htmlFor="checboxView">View only</label>
+                    <input
+                        type="checkbox"
+                        onChange={changeCheckboxApp}
+                        id="checboxView"
+                        className="checkboxView"
+                    />
+                </div>
+                <button
+                    onClick={deleteHandler}
+                    className="deleteButton"
+                    style={{ marginLeft: 10 }}
+                >
+                    Delete selected cards
+                </button>
+            </header>
+            <div className="cardContainer">
+                <CardList
+                    onChecked={checked}
+                    items={data}
+                    checkBoxChange={changeActiveHandler}
+                />
+            </div>
         </div>
-        <button
-          onClick={deleteHandler}
-          className="deleteButton"
-          style={{ marginLeft: 10 }}
-        >
-          Delete selected cards
-        </button>
-      </header>
-      <div className="cardContainer">
-        <CardList
-          onChecked={checked}
-          items={data}
-          checkBoxChange={changeActiveHandler}
-        />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/CardsComponent/CardBody.js
+++ src/CardsComponent/CardBody.js
@@ -1,13 +1,13 @@
 function CardBody({ editing, checked, value, onChange }) {
-  return editing ? (
-    <div className={checked ? "yellowBox textDiv" : "borderBox textDiv"}>
-      <textarea onChange={onChange} className="textAreaBox" value={value} />
-    </div>
-  ) : (
-    <div className={checked ? "yellowBox textDiv" : "borderBox textDiv"}>
-      {value}
-    </div>
-  );
+    return editing ? (
+        <div className={checked ? 'yellowBox textDiv' : 'borderBox textDiv'}>
+            <textarea onChange={onChange} className="textAreaBox" value={value} />
+        </div>
+    ) : (
+        <div className={checked ? 'yellowBox textDiv' : 'borderBox textDiv'}>
+            {value}
+        </div>
+    );
 }
 
 export default CardBody;
