57d48d062c08572d0cd782d702d4033cffc63aa0
mishanya1111 <mihail0grebenkin@gmail.com>
2024-10-15 18:52:41 +0300

    изменил withLoadingDelay(не добавил в comit)

--- src/hooks/withLoadingDelay.js
+++ src/hooks/withLoadingDelay.js
@@ -1,27 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from "react";
+import { CardContext } from "../context/сardArrayContext";
 
 function withLoadingDelay(WrappedComponent) {
-    return function WithDelayComponent(props) {
-        const [isLoading, setIsLoading] = useState(true);
+  return function WithDelayComponent(props) {
+    /*
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
 
-        useEffect(() => {
-            const timer = setTimeout(() => {
-                setIsLoading(false);
-            }, 2000);
+      return () => clearTimeout(timer);
+    }, []);*/
+    const { fetchingDate } = useContext(CardContext);
+    if (fetchingDate) {
+      return (
+        <div className="spinBox">
+          <div className="spinner"></div>
+        </div>
+      );
+    }
 
-            return () => clearTimeout(timer);
-        }, []);
-
-        if (isLoading) {
-            return (
-                <div className="spinBox">
-                    <div className="spinner"></div>
-                </div>
-            );
-        }
-
-        return <WrappedComponent {...props} />;
-    };
+    return <WrappedComponent {...props} />;
+  };
 }
 
 export default withLoadingDelay;
