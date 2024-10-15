b7e0d394bd6c41c40438cb134010de4674121623
mishanya1111 <mihail0grebenkin@gmail.com>
2024-10-15 18:43:01 +0300

    Вывод ошибки через alert, изменил withLoadingDelay но оно работает не так как я думал. Я думал оно будет долго грузить данные если их много, а по факту оно вообще не показывает сайт пока всё не скачается

--- src/context/сardArrayContext.js
+++ src/context/сardArrayContext.js
@@ -1,80 +1,94 @@
-import { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 export const CardContext = createContext({
-    items: [],
-    cardCheckBoxClick: () => {},
-    viewOnlyCheckBoxClick: () => {},
-    addNewCard: () => {},
-    deleteSelectedCard: () => {},
-    viewOnly: false
+  items: [],
+  cardCheckBoxClick: () => {
+  },
+  viewOnlyCheckBoxClick: () => {
+  },
+  addNewCard: () => {
+  },
+  deleteSelectedCard: () => {
+  },
+  viewOnly: false,
+  fetchingDate:false
 });
 
 export default function CardContextProvider({ children }) {
-    const [checked, setChecked] = useState(false);
-    const [data, setData] = useState([]);
-    useEffect(() => {
+  const [checked, setChecked] = useState(false);
+  const [data, setData] = useState([]);
+  const [fetching,setFetching] = useState(true)
+  useEffect(() => {
+    async function fetchData() {
+      try {
         const apiUrl =
-            'https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json';
-        axios.get(apiUrl).then(resp => {
-            const allCards = resp.data;
-            const resData = allCards
-                .filter(el => el.Number < 16)
-                .map(el => {
-                    return {
-                        id: el.Number,
-                        title: el.Name,
-                        text: el.About,
-                        isActive: false
-                    };
-                });
-            setData(resData);
-        });
-    }, []);
-
-    function changeActiveHandler(id) {
-        setData(cards =>
-            cards.map(card => {
-                if (card.id === id) {
-                    const prev = { ...card };
-                    prev.isActive = !card.isActive;
-                    return prev;
-                }
-                return card;
-            })
-        );
+          "https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json";
+        const resp = await axios.get(apiUrl);
+        const allCards = resp.data;
+        const resData = allCards
+          .slice(0, 15)
+          .map(el => {
+            return {
+              id: el.Number,
+              title: el.Name,
+              text: el.About,
+              isActive: false
+            };
+          });
+        setData(resData);
+        setFetching(false);
+      } catch (err) {
+        alert(`The data was not fetch\nError message:${err.message}`);
+      }
     }
+    fetchData();
+  }, []);
 
-    function deleteHandler() {
-        setData(card => card.filter(el => !el.isActive));
-    }
-
-    function changeCheckboxApp() {
-        setChecked(!checked);
-    }
-
-    function addNewCard() {
-        setData([
-            {
-                id: uuidv4(),
-                title: '',
-                text: '',
-                isActive: false
-            },
-            ...data
-        ]);
-    }
-
-    const contextValue = {
-        items: data,
-        cardCheckBoxClick: changeActiveHandler,
-        viewOnlyCheckBoxClick: changeCheckboxApp,
-        addNewCard: addNewCard,
-        deleteSelectedCard: deleteHandler,
-        viewOnly: checked
-    };
-    return (
-        <CardContext.Provider value={contextValue}>{children}</CardContext.Provider>
+  function changeActiveHandler(id) {
+    setData(cards =>
+      cards.map(card => {
+        if (card.id === id) {
+          const prev = { ...card };
+          prev.isActive = !card.isActive;
+          return prev;
+        }
+        return card;
+      })
     );
+  }
+
+  function deleteHandler() {
+    setData(card => card.filter(el => !el.isActive));
+  }
+
+  function changeCheckboxApp() {
+    setChecked(!checked);
+  }
+
+  function addNewCard() {
+    setData([
+      {
+        id: uuidv4(),
+        title: "",
+        text: "",
+        isActive: false
+      },
+      ...data
+    ]);
+  }
+
+  const contextValue = {
+    items: data,
+    cardCheckBoxClick: changeActiveHandler,
+    viewOnlyCheckBoxClick: changeCheckboxApp,
+    addNewCard: addNewCard,
+    deleteSelectedCard: deleteHandler,
+    viewOnly: checked,
+    fetchingDate:fetching
+  };
+  return (
+    <CardContext.Provider value={contextValue}>{children}</CardContext.Provider>
+  );
 }
